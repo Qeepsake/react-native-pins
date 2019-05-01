d971d5db7994d1516699609674b7e71582573dee
Luke Brandon Farrell <lukebrandonfarrell@gmail.com>
2019-05-01 11:58:17 +0200

    First commit

---
+++ PinInput.js
@@ -0,0 +1,173 @@
+/**
+ * @author Luke Brandon Farrell
+ * @description Pin Input component with shake animation
+ */
+
+import React, { Component } from "react";
+import { View, Animated, StyleSheet, Vibration } from "react-native";
+import PropTypes from "prop-types";
+
+class PinInput extends Component {
+  /**
+   * [ Built-in React method. ]
+   *
+   * Allows us to render JSX to the screen
+   */
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      shake: new Animated.Value(0)
+    };
+  }
+
+  /**
+   * [ Built-in React method. ]
+   *
+   * Executed when the component is mounted to the screen.
+   */
+  componentDidMount() {
+    this.props.onRef(this);
+  }
+
+  /**
+   * [ Built-in React method. ]
+   *
+   * Executed when the component is unmounted from the screen
+   */
+  componentWillUnmount() {
+    this.props.onRef(undefined);
+  }
+
+  /**
+   * [ Built-in React method. ]
+   *
+   * Allows us to render JSX to the screen
+   */
+  render() {
+    /** Styles */
+    const {
+      containerDefaultStyle,
+      pinDefaultStyle,
+      pinActiveDefaultStyle
+    } = styles;
+    /** Props */
+    const {
+      numberOfPinsActive,
+      numberOfPins,
+      // Style Props
+      containerStyle,
+      pinStyle,
+      pinActiveStyle
+    } = this.props;
+    /** State */
+    const { shake } = this.state;
+
+    // Create the pins from set props
+    const pins = [];
+
+    for (let p = 0; p < numberOfPins; p++) {
+      pins.push(
+      <View
+      key={p}
+      style={[
+            pinDefaultStyle,
+        pinStyle,
+        p < numberOfPinsActive
+          ? { ...pinActiveDefaultStyle, ...pinActiveStyle }
+          : {}
+    ]}
+      />
+    );
+    }
+
+    // Create the shake animation via interpolation
+    const shakeAnimation = shake.interpolate({
+      inputRange: [0, 0.2, 0.4, 0.6, 0.8, 1],
+      outputRange: [0, -20, 20, -20, 20, 0]
+    });
+
+    return (
+      <Animated.View
+          style={[
+                containerDefaultStyle,
+            containerStyle,
+          { left: shakeAnimation }
+        ]}
+      >
+        {pins}
+      </Animated.View>
+    );
+  }
+
+  /**
+   * Shakes the pins
+   */
+  shake() {
+    // If vibration is enabled then we vibrate on error
+    if (this.props.vibration) {
+      Vibration.vibrate(500);
+    }
+
+    // Reset animation to so we can reanimate
+    this.state.shake.setValue(0);
+
+    // Animate the pins to shake
+    Animated.spring(this.state.shake, { toValue: 1 }).start(() => {
+      if (this.props.animationShakeCallback) {
+        this.props.animationShakeCallback();
+      }
+    });
+  }
+}
+
+PinInput.propTypes = {
+  onRef: PropTypes.any.isRequired,
+  numberOfPins: PropTypes.number,
+  numberOfPinsActive: PropTypes.number,
+  vibration: PropTypes.bool,
+  animationShakeCallback: PropTypes.func,
+  // Style props
+  containerStyle: PropTypes.object,
+  pinStyle: PropTypes.object,
+  pinActiveStyle: PropTypes.object
+};
+
+PinInput.defaultProps = {
+  // Number of pins to create
+  numberOfPins: 5,
+  // Active number of pins
+  numberOfPinsActive: 0,
+  // Is vibration enabled or disabled
+  vibration: true
+};
+
+export default PinInput;
+
+/** -------------------------------------------- */
+/**             Component Styling                */
+/** -------------------------------------------- */
+const styles = StyleSheet.create({
+  // Style for pin container. You can use the flex
+  // property to expand the pins to take up more space
+  // on the screen. The default is 0.8.
+  containerDefaultStyle: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingTop: 25,
+    paddingBottom: 25
+  },
+  pinDefaultStyle: {
+    width: 18,
+    height: 18,
+    marginRight: 15,
+    marginLeft: 15,
+    borderRadius: 9,
+    opacity: 0.45,
+    backgroundColor: "#FFF"
+  },
+  pinActiveDefaultStyle: {
+    opacity: 1.0
+  }
+});
---
+++ README.md
@@ -0,0 +1,58 @@
+# react-native-pins
+
+<img src="https://raw.githubusercontent.com/LukeBrandonFarrell/open-source-images/master/react-native-awesome-pin/pins.png" width="50%" /><br />
+
+The pins come with a shake animation and configurable 
+device vibration.
+
+## Install
+
+To get started install via npm:
+```sh
+ npm install react-native-pins
+```
+
+#### Usage
+
+Import:
+```js
+ import { PinInput } from 'react-native-awesome-pin';
+```
+
+Then add it to your code:
+```js
+<PinInput
+    onRef={ref => (this.pins = ref)}
+    numberOfPins={5}
+    numberOfPinsActive={2}
+/>
+```
+
+The `<PinInput />` has a `shake()` method which can be called through a reference `this.pins.shake()`.
+This will perform a shake animation and vibration if enabled. A callback can be passed through props which
+will be fired when the animation is complete. See props below.
+
+#### PinInput
+
+| Prop            | Type          | Optional  | Default              | Description                                                                             |
+| --------------- | ------------- | --------- | -------------------- | --------------------------------------------------------------------------------------- |
+| onRef           | any           | No        |                      | onRef allows you to call the `shake()` method.                              |
+| numberOfPins    | number        | Yes       | 5                    | Number of pins to render.                                                               |
+| numberOfPinsActive | number     | Yes       | 0                    | Number of active pins. You can pass the `pin.length` here.                              |
+| vibration       | bool          | Yes       | true                 | Should vibration be enabled on shake?                                                   |
+| animationShakeCallback | func   | Yes       |                      | A callback triggered when the pin shake animation has finished.                         |
+| containerStyle  | object        | Yes       | See PinInput.js      | Style applied to PINS container.                                                        |
+| pinStyle        | object        | Yes       | See PinInput.js      | Style applied to each circle PIN.                                                       |
+| pinActiveStyle  | object        | Yes       | See PinInput.js      | Style applied to each circle PIN when it is active.                                     |
+
+## Contributing
+
+If you want to issue a PR, go ahead ;)
+
+## Authors
+
+* [**Luke Brandon Farrell**](https://lukebrandonfarrell.com/) - *Author*
+
+## License
+
+This project is licensed under the MIT License
